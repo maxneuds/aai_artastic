f984162c74eb44c49aa443e67491bb80b1d4ff9f
Nico  Puhlmann <puhlmann7@googlemail.com>
2020-05-26 15:52:50 +0200

    First Components integrated so that we will have a basis home layout.

---
+++ artastic/frontend/src/components/TheNavbar.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app-bar
+    absolute
+    color="#6A76AB"
+    dark
+    shrink-on-scroll
+    prominent
+    src="https://picsum.photos/1920/1080?random"
+    scroll-target="#scrolling-techniques-5"
+  >
+    <template v-slot:img="{ props }">
+      <v-img v-bind="props" gradient="to top right, rgba(100,115,201,.7), rgba(25,32,72,.7)"></v-img>
+    </template>
+
+    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    <v-spacer />
+    <v-toolbar-title id="title" class="title">Artastic!</v-toolbar-title>
+    <v-spacer />
+  </v-app-bar>
+</template>
+
+<style scoped>
+#title {
+  font-family: unquote("Roboto");
+}
+</style>
