980a0dbbc750b9dd06d8b689994ff13c337964c3
Nico  Puhlmann <puhlmann7@googlemail.com>
2020-06-25 13:26:11 +0200

    added OPTIONAL option for querys to get all data thats available

--- artastic/website/services.py
+++ artastic/website/services.py
@@ -16,12 +16,12 @@ def get_artwork(data):
             ?a rdf:type :artwork ;
             rdfs:label "{0}" ;
             rdfs:label ?label;
-            :description ?description;
-            :abstract ?abstract;
-            :image ?image;
-            :artist/rdfs:label ?artist;
-            :movement/rdfs:label ?movement;
-            :material/rdfs:label ?material;
+            OPTIONAL{{?a :description ?description}}
+            OPTIONAL{{?a :abstract ?abstract}}
+            OPTIONAL{{?a :image ?image}}
+            OPTIONAL{{?a :artist/rdfs:label ?artist}}
+            OPTIONAL{{?a :movement/rdfs:label ?movement}}
+            OPTIONAL{{?a :material/rdfs:label ?material}}
         }}
     """.format(label)
     sparql.setQuery(query_string)
@@ -50,16 +50,16 @@ def get_artist(data):
             ?p rdf:type :person ;
             rdfs:label ?label;
             rdfs:label "{0}";
-            :description ?description;
-            :image ?image;
-            :abstract ?abstract;
-            :wikipediaLink ?wikilink;
-            :gender ?gender;
-            :date_of_birth ?dateOfBirth;
-            :date_of_death ?dateOfDeath;
-            :place_of_birth ?placeOfBirth;
-            :place_of_death ?placeOfDeath;
-            :movement ?movement;
+            OPTIONAL{{?p :description ?description}}
+            OPTIONAL{{?p :image ?image}}
+            OPTIONAL{{?p :abstract ?abstract}}
+            OPTIONAL{{?p :wikipediaLink ?wikilink}}
+            OPTIONAL{{?p :gender ?gender}}
+            OPTIONAL{{?p :date_of_birth ?dateOfBirth}}
+            OPTIONAL{{?p :date_of_death ?dateOfDeath}}
+            OPTIONAL{{?p :place_of_birth ?placeOfBirth}}
+            OPTIONAL{{?p :place_of_death ?placeOfDeath}}
+            OPTIONAL{{?p :movement ?movement}}
         }}
 
     """ .format(label)
@@ -88,14 +88,14 @@ def get_location(data):
         WHERE {{ ?a rdf:type :location ;
                 rdfs:label ?label ;
                 rdfs:label "{0}" ;
-                :description ?description;
-                :abstract ?abstract;
-                :wikipediaLink ?wikipediaLink;
-                :image ?image;
-                :country ?country;
-                :website ?website;
-                :lat ?lat;
-                :lon ?lon.
+                OPTIONAL {{?a :description ?description}}
+                OPTIONAL {{?a :abstract ?abstract}}
+                OPTIONAL {{?a :wikipediaLink ?wikipediaLink}}
+                OPTIONAL {{?a :image ?image}}
+                OPTIONAL {{?a :country ?country}}
+                OPTIONAL {{?a :website ?website}}
+                OPTIONAL {{?a :lat ?lat}}
+                OPTIONAL {{?a :lon ?lon.}}
         }}
 
     """ .format(label)
@@ -124,10 +124,10 @@ def get_standard_entity_data(data):
         WHERE {{ ?a rdf:type :{1} ;
                 rdfs:label ?label ;
                 rdfs:label "{0}" ;
-                :description ?description;
-                :abstract ?abstract;
-                :wikipediaLink ?wikipediaLink;
-                :image ?image;
+                OPTIONAL {{?a :description ?description}}
+                OPTIONAL {{?a :abstract ?abstract}}
+                OPTIONAL {{?a :wikipediaLink ?wikipediaLink}}
+                OPTIONAL {{?a :image ?image}}
         }}
 
     """ .format(label, obj_class)
