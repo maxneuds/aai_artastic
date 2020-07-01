f390408711f260768e70cec4b13e782288f8a086
Nico  Puhlmann <puhlmann7@googlemail.com>
2020-07-01 15:00:31 +0200

    SoundButton now with play/pause and destroy on reload.

--- artastic/frontend/src/components/CardList.vue
+++ artastic/frontend/src/components/CardList.vue
@@ -39,7 +39,7 @@
               >Read more</v-btn>
             </router-link>
             <v-spacer></v-spacer>
-            <SoundButton :text="card.Abstract" />
+            <SoundButton :text="card.Abstract" v-bind:key="card.Abstract" />
           </v-card-actions>
         </v-card>
       </v-col>
